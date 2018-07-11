7c6a47284f1e41a993a06d965d7401d77a472a2e
rhalid <rhalid.bouakhris@gmail.com>
2018-07-11 22:01:44 +0200

    fetch + axios

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-          </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="/" className="left brand-logo">Logo</a>
+                    <ul className="right">
+                        <li><a href="/auth/google/">Login with Google</a></li>
+                    </ul>
+                </div>
+            </nav >
         );
     }
-
 }
 
 export default Header;
